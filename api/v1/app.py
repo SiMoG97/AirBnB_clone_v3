df80e6116ccd6485759793c5721b89bf93bf32ae
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-10-30 08:32:27 -0700

    tasks 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,8 +14,8 @@ app.register_blueprint(app_views)
 def teardown(exception):
     storage.close()
 
-
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
+
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ flask """
-from api.v1.views.index import *
+"""from api.v1.views.index import *"""
+from api.v1.views.index import status
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
