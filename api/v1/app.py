3b7a833e4f2672de5240ded771eadc01a2ce71e3
SiMoG97 <echaaranimohamed@gmail.com>
2023-10-30 18:11:43 +0100

    updating getenv app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ task3"""
-import os 
+from os import getenv
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
@@ -12,10 +12,11 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception):
+    """Teardown context"""
     storage.close()
 
 
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
